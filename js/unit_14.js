4d4719111f54eddd793dbde6db4ac940a855ce86
risedack1 <risedack1@gmail.com>
2022-07-18 22:43:37 +0300

    added select city

--- js/unit_14.js
+++ js/unit_14.js
@@ -1,13 +1,27 @@
 // modal--------------------------------------------
 
 const modalWrapper = document.querySelector('.modal');
+const modalQuestion = document.querySelector('.modal__inner--question');
+const modalSearch = document.querySelector('.modal__inner--search');
+const openModalButton = document.querySelector('.new-country-button');
 
 modalWrapper.addEventListener('click', (e) => {
     const target = e.target;
 
-    if (target.matches('.modal__button') || target.matches('.modal__close')) {
+    if (target.matches('.modal__button--yes') || target.matches('.modal__close')) {
         modalWrapper.classList.add('modal--disable');
     }
+
+    if (target.matches('.modal__button--no')) {
+        modalQuestion.classList.add('modal__inner--disable');
+        modalSearch.classList.remove('modal__inner--disable');
+    }
+});
+
+openModalButton.addEventListener('click', () => {
+    modalWrapper.classList.remove('modal--disable');
+    modalQuestion.classList.add('modal__inner--disable');
+    modalSearch.classList.remove('modal__inner--disable');
 });
 
 // set weather---------------------------------------------------------
@@ -15,14 +29,14 @@ modalWrapper.addEventListener('click', (e) => {
 const param = {
     url: 'https://api.openweathermap.org/data/2.5/forecast',
     appid: 'ff4384d9d6ad00be68b96672b35cd33c',
-}
+};
 
 function getLocation() {
     // get location of country
     fetch("https://ipinfo.io/json?token=1757608293c75d").then(
         (response) => response.json()
     ).then(getWeather)
-        .then(addCityName)
+        .then(addCityName);
 }
 
 function addCityName(dataLoc) {
@@ -35,9 +49,12 @@ function addCityName(dataLoc) {
 }
 
 function getWeather(dataLoc) {
-    const locArray = dataLoc.loc.split(',');
+    const cityName = dataLoc.city;
+    const countryName = dataLoc.country;
 
-    fetch(`${param.url}?lat=${locArray[0]}&lon=${locArray[1]}&units=metric&APPID=${param.appid}`)
+    console.log(dataLoc);
+
+    fetch(`${param.url}?q=${cityName},${countryName.toLowerCase()}&units=metric&APPID=${param.appid}`)
         .then(weather => {
             return weather.json();
         }).then(showWeather);
@@ -46,44 +63,51 @@ function getWeather(dataLoc) {
 }
 
 function showWeather(data) {
-    console.log(data);
+    // console.log(data);
 
-    const sity = document.querySelector('.weather-screen__sity');
+    const city = document.querySelector('.weather-screen__city');
     const temperature = document.querySelector('.weather-screen__temperature');
     const caption = document.querySelector('.weather-screen__caption');
     const icon = document.querySelector('.weather__icon img');
-    const realFeel = document.querySelector('#real-feel')
-    const humidity = document.querySelector('#humidity')
-    const wind = document.querySelector('#wind')
-    const pressure = document.querySelector('#pressure')
+    const realFeel = document.querySelector('#real-feel');
+    const humidity = document.querySelector('#humidity');
+    const wind = document.querySelector('#wind');
+    const pressure = document.querySelector('#pressure');
 
     // future list
     const futureWeather = document.querySelectorAll('.weather-future__item');
 
-    sity.textContent = data.city.name;
-    temperature.innerHTML = Math.round(data.list['0'].main.temp) + '&#xb0; C';
-    caption.textContent = data.list['0'].weather['0']['main'];
-    icon.setAttribute('src', `img/${data.list['0'].weather['0']['main'].toLowerCase()}.svg`);
+    if (data.city) {
+        city.textContent = data.city.name;
+        temperature.innerHTML = Math.round(data.list['0'].main.temp) + '&#xb0; C';
+        caption.textContent = data.list['0'].weather['0']['main'];
+        icon.setAttribute('src', `img/${data.list['0'].weather['0']['main'].toLowerCase()}.svg`);
 
-    // future listsub info
-    realFeel.innerHTML = Math.round(data.list['0'].main.feels_like) + '&#xb0; C';
-    humidity.textContent = data.list['0'].main.humidity + '%';
-    wind.textContent = data.list['0'].wind.speed;
-    pressure.textContent = data.list['0'].main.pressure + 'hPa';
+        // future listsub info
+        realFeel.innerHTML = Math.round(data.list['0'].main.feels_like) + '&#xb0; C';
+        humidity.textContent = data.list['0'].main.humidity + '%';
+        wind.textContent = data.list['0'].wind.speed;
+        pressure.textContent = data.list['0'].main.pressure + 'hPa';
 
-    // feed future weather
+        // feed future weather
 
-    for (let i = 0; i < futureWeather.length; i++) {
-        const hourse = futureWeather[i].querySelector('.weather-small__hourse');
-        const icon = futureWeather[i].querySelector('.weather-small__icon img');
-        const temperature = futureWeather[i].querySelector('.weather-small__temperature');
+        for (let i = 0; i < futureWeather.length; i++) {
+            const hourse = futureWeather[i].querySelector('.weather-small__hourse');
+            const icon = futureWeather[i].querySelector('.weather-small__icon img');
+            const temperature = futureWeather[i].querySelector('.weather-small__temperature');
 
-        // get future time
-        const time = data.list[i].dt_txt;
+            // get future time
+            const time = data.list[i].dt_txt;
 
-        hourse.textContent = time.slice(-8, -3);
-        icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[i].weather['0'].icon}@2x.png`);
-        temperature.innerHTML = Math.round(data.list[i].main.temp) + '&#xb0; C';
+            hourse.textContent = time.slice(-8, -3);
+            icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[i].weather['0'].icon}@2x.png`);
+            temperature.innerHTML = Math.round(data.list[i].main.temp) + '&#xb0; C';
+        }
+
+
+    } else {
+        errorSwitcher = 0;
+        alert('Incorrect city, try again');
     }
 
 }
